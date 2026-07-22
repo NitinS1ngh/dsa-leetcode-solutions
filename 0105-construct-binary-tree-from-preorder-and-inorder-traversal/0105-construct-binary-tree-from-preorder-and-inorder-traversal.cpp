22cb0f947956706e73d0b7f8bd96d4d72d1fe8ec
Nitin Singh <ns23241802@gmail.com>
2026-07-22 11:48:11 +0530

    Time: 3 ms (72.88%), Space: 27.5 MB (54.78%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,27 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx){
+    unordered_map<int,int> mp;
+    TreeNode* solve(vector<int>& preorder, int start, int end, int& idx){
         if(start>end) return nullptr;
 
-        int rootval = preorder[idx];
-        int i = start;
-        for( ; i<=end; i++){
-            if(inorder[i] == rootval) break;
-        }
-        idx++;
+        int rootval = preorder[idx++];
+        int mid = mp[rootval];
 
         TreeNode* root = new TreeNode(rootval);
-        root->left = solve(preorder,inorder,start,i-1,idx);
-        root->right = solve(preorder,inorder,i+1,end,idx);
+        root->left = solve(preorder,start,mid-1,idx);
+        root->right = solve(preorder,mid+1,end,idx);
 
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
-        int idx = 0;
-        return solve(preorder,inorder,0,n-1,idx);
-    }
 
-    // the TC is O(n^2) which can be optimised by mapping the inorder
+        for(int i=0; i<n; i++){
+            mp[inorder[i]] = i;
+        }
+
+        int idx = 0;
+        return solve(preorder,0,n-1,idx);
+    }
 };
